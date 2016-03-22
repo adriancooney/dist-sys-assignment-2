e3c33bdea90a13bf8b13624b2e51dadcc9c426e2
Seán Collum <scseano@gmail.com>
2016-03-22 22:36:35 +0000

    Fixed Input. It works ;)

--- src/MapReducePooled.java
+++ src/MapReducePooled.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -7,83 +8,41 @@ import java.util.concurrent.Executors;
 
 public class MapReducePooled {
 
+    private static Map readFile(File file) throws IOException {
+        Map<String, String> input = new HashMap<String, String>();
+        StringBuilder fileContents = new StringBuilder((int)file.length());
+        String filename = file.getName();
+        Scanner scanner = new Scanner(file);
+        String lineSeparator = System.getProperty("line.separator");
+
+        try {
+            while(scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine() + lineSeparator);
+            }
+        } finally {
+            scanner.close();
+        }
+        input.put(filename,fileContents.toString());
+        return input;
+    }
+
     public static void main(String[] args) {
 
         List<File> fileList = new LinkedList<File>();
         List<Map<String,String>> cmdLineInput = new LinkedList<Map<String,String>>();
-        Map<String, String> test = new HashMap<String, String>();
 
-        //File directories are passed from command line and Files are created.
-        //Each file is then mapped by following the below example with
-        //"file1.txt", "foo foo bar cat dog dog" etc.
-        //Map is added to a List of Maps.
-        if(args.length>0)
-        {
-            for(int i=0;i<args.length;i++)
-            {
-                File file = new File(args[i]);
-                fileList.add(file);
-            }
+        File file1 = new File(args[0]);
+        //File file2 = new File(args[1]);
+        //File file3 = new File(args[2]);
+
+        Map<String, String> input1 = new HashMap<String, String>();
+        try {
+            input1 = readFile(file1);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        //read the content of each file
-        //into one string
-        //Map the File to the string.
-        for(File file : fileList)
-        {
-            String content = null ;
-            String name = file.getName();
-            String newLine = System.getProperty("line.separator");
-            Scanner read = null;
-            try {
-                read = new Scanner(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            read.useDelimiter(newLine);
-            while (read.hasNext())
-            {
-                content = read.next();
-            }
-            read.close();
-
-            test.put(name, content);
-            cmdLineInput.add(test);
-            test.clear();
-        }
-
-        // the problem:
-
-        // from here (INPUT)
-
-        // "file1.txt" => "foo foo bar cat dog dog"
-        // "file2.txt" => "foo house cat cat dog"
-        // "file3.txt" => "foo foo foo bird"
-
-        // we want to go to here (OUTPUT)
-
-        // "foo" => { "file1.txt" => 2, "file3.txt" => 3, "file2.txt" => 1 }
-        // "bar" => { "file1.txt" => 1 }
-        // "cat" => { "file2.txt" => 2, "file1.txt" => 1 }
-        // "dog" => { "file2.txt" => 1, "file1.txt" => 2 }
-        // "house" => { "file2.txt" => 1 }
-        // "bird" => { "file3.txt" => 1 }
-
-        // in plain English we want to
-
-        // Given a set of files with contents
-        // we want to index them by word
-        // so I can return all files that contain a given word
-        // together with the number of occurrences of that word
-        // without any sorting
-
-        ////////////
-        // INPUT:
-        ///////////
-
-        Map<String, String> input = new HashMap<String, String>();
-        input.put("file1.txt", "foo foo bar cat dog dog");
-        input.put("file2.txt", "foo house cat cat dog");
-        input.put("file3.txt", "foo foo foo bird");
+        Map<String, String> input2 = new HashMap<String, String>();
+        Map<String, String> input3 = new HashMap<String, String>();
 
 
         // APPROACH #3: Distributed MapReduce
@@ -103,7 +62,7 @@ public class MapReducePooled {
             }
         };
 
-        Iterator<Map.Entry<String, String>> inputIter = input.entrySet().iterator();
+        Iterator<Map.Entry<String, String>> inputIter = input1.entrySet().iterator();
         while(inputIter.hasNext()) {
             Map.Entry<String, String> entry = inputIter.next();
             final String file = entry.getKey();
@@ -161,6 +120,8 @@ public class MapReducePooled {
         System.out.println(output);
     }
 
+
+
     public interface MapCallback<E, V> {
         void mapDone(E key, List<V> values);
     }
@@ -213,6 +174,8 @@ public class MapReducePooled {
         public String toString() {
             return "[\"" + word + "\",\"" + file + "\"]";
         }
+
+
     }
-} 
+}
 
